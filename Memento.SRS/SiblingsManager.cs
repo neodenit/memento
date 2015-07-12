d2571a080cb4223ee3bc493a37b5a641a0b27c2a
Denis Nekrylov <neodenit@gmail.com>
2015-07-12 22:06:32 +0300

    Add sibling cards manager stub

---
+++ Memento.SRS/SiblingsManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.SRS
+{
+    public static class SiblingsManager
+    {
+        public static void RearrangeSiblings(IDeck deck, IEnumerable<ICard> cards)
+        {
+
+        }
+    }
+}
