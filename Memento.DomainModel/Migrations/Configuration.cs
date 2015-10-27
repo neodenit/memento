6be8be34fa8786f612db2333d06cccd0181830a1
Denis Nekrylov <neodenit@gmail.com>
2015-10-27 22:19:47 +0300

    Fix migrations

--- Memento.DomainModel/Migrations/Configuration.cs
+++ Memento.DomainModel/Migrations/Configuration.cs
@@ -1,19 +1,18 @@
-namespace Memento.Web.Migrations
+namespace Memento.DomainModel.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Memento.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Memento.DomainModel.Models.MementoContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "Memento.Models.ApplicationDbContext";
         }
 
-        protected override void Seed(Memento.Models.ApplicationDbContext context)
+        protected override void Seed(Memento.DomainModel.Models.MementoContext context)
         {
             //  This method will be called after migrating to the latest version.
 
